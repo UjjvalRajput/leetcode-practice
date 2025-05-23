224db713d2a9e00a316ba81f8eeaec232bfabc97
Ujjval Rajput <101949411+UjjvalRajput@users.noreply.github.com>
2025-05-22 20:21:25 -0400

    Time: 175 ms (75.21%), Space: 23.6 MB (78.12%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        basket = {} # fruit_type -> times_seen
+        max_len = 0
+        start = 0
+
+        for end in range(len(fruits)):
+            fruit = fruits[end]
+            basket[fruit] = basket.get(fruit, 0) + 1 # new entry or increment
+            while len(basket) > 2:
+                basket[fruits[start]] -= 1
+                if basket[fruits[start]] == 0:
+                    del basket[fruits[start]]
+                start += 1
+            max_len = max(max_len, end - start + 1)
+
+        return max_len
+
