583020ebe6230963bc90d9372a6ce5776cdac935
Ujjval Rajput <101949411+UjjvalRajput@users.noreply.github.com>
2025-06-09 22:47:25 -0400

    Time: 179 ms (68.4%), Space: 23.5 MB (77.89%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -1,15 +1,14 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         start = 0
+        accumulator = 0
         state = {}
-        max_fruit = 0
-
         for end in range(len(fruits)):
-            state[fruits[end]] = state.get(fruits[end], 0) + 1
+            state[fruits[end]] = state.get(fruits[end], 0) + 1 # {2:1, 4:2, 5:3, ...,}
             while len(state) > 2:
                 state[fruits[start]] -= 1
                 if state[fruits[start]] == 0:
                     del state[fruits[start]]
                 start += 1
-            max_fruit = max(max_fruit, end - start + 1)
-        return max_fruit
+            accumulator = max(accumulator, end - start + 1) # len([2, 3, 4, 4]) = (index 3 - index 0) + 1 = 4
+        return accumulator
