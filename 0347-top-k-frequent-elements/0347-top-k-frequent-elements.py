220cb7bfcc0b20b227a2d6175d2585666f45d66f
Ujjval Rajput <101949411+UjjvalRajput@users.noreply.github.com>
2025-06-09 23:41:42 -0400

    Time: 7 ms (52.07%), Space: 21.2 MB (79.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,21 @@
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # count the frequencies of each number in nums
+        # naive solution would sort by values and return k most appearing elements in O(n log n)
         acc = {}
         for num in nums:
-            acc[num] = acc.get(num, 0) + 1
+            acc[num] = acc.get(num, 0) + 1 # replace with 0 if key not found 
 
         min_heap = []
-        for key, value in acc.items():
+        # we don't need to sort all n elements, just what is required. So:
+        # maintain a min heap of size k, pop the least valued element in the min heap everytime min heap's  
+        # size gets bigger than k. This is a O(n log k) operation where k is size of heap and n is len(nums)
+        # at the end return the keys of all elements in the heap as those are the most frequent k values
+        # because the least frequent values would be swapped out as heap is of size k.
+        for key, value in acc.items(): 
             heapq.heappush(min_heap, (value, key))
             if len(min_heap) > k:
                 heapq.heappop(min_heap)
 
-        return [i[1] for i in min_heap]
+        return [i[1] for i in min_heap] # return the keys (actual numbers and not their values)
